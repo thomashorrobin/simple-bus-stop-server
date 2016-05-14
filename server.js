21fadd126141e96bda1c7103fe819aaab3266351
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-14 21:00:24 +1200

    added search functionality to the super simple server

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
 var http = require('http');
+var url = require('url');
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://104.131.179.136:27017/busdb';
+var dburl = 'mongodb://104.131.179.136:27017/busdb';
 var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
-    MongoClient.connect(url, function(err, db) {
+    let query = url.parse(req.url, true);
+    if (query.pathname == '/stops') {
+        if (query.query.search) {
+            //res.end(JSON.stringify(query));
+            search(query.query.search, res)
+        } else {
+            list(res);
+        }
+    } else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: 'Path not found.'}));
+    }
+}).listen(port);
+
+function list(res) {
+    MongoClient.connect(dburl, function(err, db) {
+        if (err) {
+            res.statusCode = 500
+            res.end(JSON.stringify(err));
+        }
         db.collection('stops').find({}).toArray(function (err, docs) {
             if (err == null) {
                 res.end(JSON.stringify(docs));
             } else {
+                res.statusCode = 500
                 res.end("there was an error, contact Tom on 0273364706");
             }
         });
     });
-}).listen(port);
+}
+
+function search(searchTerm, res) {
+    MongoClient.connect(dburl, function(err, db) {
+        if (err) {
+            res.statusCode = 500
+            res.end(JSON.stringify(err));
+        }
+        db.collection('stops').find({}).toArray(function (err, docs) {
+            if (err == null) {
+                var stops = [];
+                for (var index = 0; index < docs.length; index++) {
+                    var stop = docs[index];
+                    console.log(stop);
+                    if (stop.Name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                        stops.push(stop);
+                    }
+                } 
+                res.end(JSON.stringify(stops));
+            } else {
+                res.statusCode = 500
+                res.end("there was an error, contact Tom on 0273364706");
+            }
+        });
+    });
+}
